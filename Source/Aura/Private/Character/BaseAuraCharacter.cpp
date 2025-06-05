76da734c9921b31289b080d39674ecda6bc7a7d1
basinch <koksal.omer.aycin@gmail.com>
2025-06-05 12:34:37 +0300

    char

--- Source/Aura/Private/Character/BaseAuraCharacter.cpp
+++ Source/Aura/Private/Character/BaseAuraCharacter.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "BaseAuraCharacter.h"
+#include "Character/BaseAuraCharacter.h"
 
 ABaseAuraCharacter::ABaseAuraCharacter()
 {
