fd042a4aee4a62c681b2c07251086a32895f2b7b
basinch <koksal.omer.aycin@gmail.com>
2025-06-05 12:21:25 +0300

    d
    d

---
+++ Source/Aura/Private/Character/BaseAuraCharacter.cpp
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseAuraCharacter.h"
+
+ABaseAuraCharacter::ABaseAuraCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void ABaseAuraCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
