b52a95460dcdb4fda31307dcb2d6b9c888f29d1c
basinch <koksal.omer.aycin@gmail.com>
2025-06-06 14:22:58 +0300

    goboln
    f

--- Source/Aura/Private/Character/BaseAuraCharacter.cpp
+++ Source/Aura/Private/Character/BaseAuraCharacter.cpp
@@ -6,6 +6,10 @@
 ABaseAuraCharacter::ABaseAuraCharacter()
 {
 	PrimaryActorTick.bCanEverTick = false;
+
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>("Weapon");
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void ABaseAuraCharacter::BeginPlay()
